7a9739560e2997cc2bc925a50149cbd15b6bd1ad
Henrique Tavares <ihenrits@gmail.com>
2025-08-21 07:48:20 -0300

    feat: update styling and structure of ActiveLink, Footer, Layout, Button components, and Tailwind CSS configuration

--- site-blog/src/components/layout/index.tsx
+++ site-blog/src/components/layout/index.tsx
@@ -1,18 +1,23 @@
-import { Footer } from "../footer";
+import { Inter } from 'next/font/google';
+import { Footer } from '../footer';
 import { Header } from '../header';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
+const inter = Inter({ subsets: ['latin'] });
+
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative flex min-h-screen flex-col dark">
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
       <Header />
-      
-      <main className="flex-1 flex flex-col mb-12">{children}</main>
 
-       <Footer />
+      <main className="flex-1 flex flex-col mb-12">{children}</main>
+      
+      <Footer />
     </div>
   );
 };
