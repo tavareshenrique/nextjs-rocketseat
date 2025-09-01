8ce20ae55d837a771e8b16f77d200481ee55b90a
Henrique Tavares <ihenrits@gmail.com>
2025-09-01 07:32:43 -0300

    feat: create BlogPage component for blog section

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { LandingPage } from '@/templates/landing-page';
-import { allPosts } from 'contentlayer/generated';
 
 export default function Home() {
-  console.debug(allPosts);
-
   return <LandingPage />;
 }
