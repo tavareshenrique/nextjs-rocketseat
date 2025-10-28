d83d1e88d3191ef9296767a586d5840468164901
Henrique Tavares <ihenrits@gmail.com>
2025-10-28 08:06:31 -0300

    feat: add calendar and popover components for enhanced appointment scheduling

--- pet-shop/src/components/appointment-form/index.tsx
+++ pet-shop/src/components/appointment-form/index.tsx
@@ -22,14 +22,25 @@ import { Input } from '@/components/ui/input';
 import z from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Dog, Phone, User } from 'lucide-react';
+import { CalendarIcon, ChevronDownIcon, Dog, Phone, User } from 'lucide-react';
 import { IMaskInput } from 'react-imask';
+import { format, startOfToday } from 'date-fns';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { cn } from '@/lib/utils';
+import { Calendar } from '../ui/calendar';
 
 const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
   petName: z.string().min(3, 'O nome do pet é obrigatório'),
   phone: z.string().min(11, 'O telefone é obrigatório'),
   description: z.string().min(3, 'A descrição é obrigatória'),
+  scheduleAt: z
+    .date({
+      error: 'A data é obrigatória',
+    })
+    .min(startOfToday(), {
+      message: 'A data não pode ser no passado',
+    }),
 });
 
 type AppointFormValues = z.infer<typeof appointmentFormSchema>;
@@ -42,6 +53,7 @@ export const AppointmentForm = () => {
       petName: '',
       phone: '',
       description: '',
+      scheduleAt: undefined,
     },
   });
 
@@ -136,8 +148,8 @@ export const AppointmentForm = () => {
                         size={20}
                       />
                       <IMaskInput
-                        placeholder="(99) 9 9999-9999"
-                        mask="(00) 0 0000-0000"
+                        placeholder="(99) 99999-9999"
+                        mask="(00) 00000-0000"
                         className="pl-10 flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 text-sm text-content-primary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-border-brand disabled:cursor-not-allowed disabled:opacity-50 hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive"
                         {...field}
                       />
@@ -163,6 +175,53 @@ export const AppointmentForm = () => {
                       {...field}
                     />
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="scheduleAt"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Data
+                  </FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className={cn(
+                            'w-full justify-between text-left font-normal bg-background-tertiary border-border-primary text-content-primary hover:bg-background-tertiary hover:border-border-secondary hover:text-content-primary focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-border-brand focus:border-border-brand focus-visible:border-border-brand',
+                            !field.value && 'text-content-secondary'
+                          )}
+                        >
+                          <div className="flex items-center gap-2">
+                            <CalendarIcon
+                              className=" text-content-brand"
+                              size={20}
+                            />
+                            {field.value ? (
+                              format(field.value, 'dd/MM/yyyy')
+                            ) : (
+                              <span>Selecione uma data</span>
+                            )}
+                          </div>
+                          <ChevronDownIcon className="opacity-50 h-4 w-4" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date < startOfToday()}
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
