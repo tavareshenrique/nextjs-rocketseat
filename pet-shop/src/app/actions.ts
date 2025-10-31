a38567eddd8d32d93a6b196cc84f0c832919e4e9
Henrique Tavares <ihenrits@gmail.com>
2025-10-31 08:05:08 -0300

    feat: implement updateAppointment function and integrate with AppointmentForm for editing appointments

--- pet-shop/src/app/actions.ts
+++ pet-shop/src/app/actions.ts
@@ -55,3 +55,51 @@ export async function createAppointment(data: AppointmentData) {
     console.log(error);
   }
 }
+
+export async function updateAppointment(id: string, data: AppointmentData) {
+  try {
+    const parsedData = appointmentSchema.parse(data);
+
+    const { scheduleAt } = parsedData;
+    const hour = scheduleAt.getHours();
+
+    const isMorning = hour >= 9 && hour < 12;
+    const isAfternoon = hour >= 13 && hour < 18;
+    const isEvening = hour >= 19 && hour < 21;
+
+    if (!isMorning && !isAfternoon && !isEvening) {
+      return {
+        error:
+          'Agendamentos só podem ser feitos entre 9h e 12h, 13h e 18 ou 19h e 21h',
+      };
+    }
+
+    const existingAppointment = await prisma.appointment.findFirst({
+      where: {
+        scheduleAt,
+        id: {
+          not: id,
+        },
+      },
+    });
+
+    if (existingAppointment) {
+      return {
+        error: 'Este horário já está reservado',
+      };
+    }
+
+    await prisma.appointment.update({
+      where: {
+        id,
+      },
+      data: {
+        ...parsedData,
+      },
+    });
+
+    revalidatePath('/');
+  } catch (error) {
+    console.log(error);
+  }
+}
