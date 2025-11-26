89b0cd4841472c5f169e15467ecf99d89c578863
Henrique Tavares <ihenrits@gmail.com>
2025-11-26 17:11:18 -0300

    feat: add DatePicker component for date selection in Home page

--- pet-shop/src/app/page.tsx
+++ pet-shop/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AppointmentForm } from "@/components/appointment-form";
+import { DatePicker } from '@/components/date-picker';
 import { PeriodSection } from '@/components/period-section';
 import { Button } from '@/components/ui/button';
 import { prisma } from '@/lib/prisma';
@@ -38,6 +39,14 @@ export default async function Home({
             Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
         </div>
+
+        <div className="hidden md:flex item-center gap-4">
+          <DatePicker />
+        </div>
+      </div>
+
+      <div className="mt-3 mb-8 md:hidden">
+        <DatePicker />
       </div>
 
       <div className="pb-24 md:pb-0">
