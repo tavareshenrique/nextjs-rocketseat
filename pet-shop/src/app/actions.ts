b177c3b2e1c4ea9a87d5c47d4780f155080ddb70
Henrique Tavares <ihenrits@gmail.com>
2025-11-26 17:06:30 -0300

    feat: add alert dialog component and integrate delete functionality in AppointmentCard

--- pet-shop/src/app/actions.ts
+++ pet-shop/src/app/actions.ts
@@ -53,6 +53,9 @@ export async function createAppointment(data: AppointmentData) {
     revalidatePath('/');
   } catch (error) {
     console.log(error);
+    return {
+      error: 'Erro ao criar o agendamento',
+    };
   }
 }
 
@@ -101,5 +104,26 @@ export async function updateAppointment(id: string, data: AppointmentData) {
     revalidatePath('/');
   } catch (error) {
     console.log(error);
+    return {
+      error: 'Erro ao atualizar o agendamento',
+    };
+  }
+}
+
+export async function deleteAppointment(id: string) {
+  try {
+    await prisma.appointment.delete({
+      where: {
+        id,
+      },
+    });
+
+    revalidatePath('/');
+  } catch (error) {
+    console.log(error);
+
+    return {
+      error: 'Erro ao remover agendamento. Tente novamente.',
+    };
   }
 }
